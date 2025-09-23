f8751b47fd1fb51baeab1101ba31fb98c4eacc73
Sergio Sacristan <sergiosacristan8@gmail.com>
2025-09-23 11:50:39 +0200

    reactive form validations

--- src/app/components/form/form.ts
+++ src/app/components/form/form.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -9,10 +9,14 @@ import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 })
 export class Form {
   contactForm: FormGroup = new FormGroup({
-    name: new FormControl(),
-    subname: new FormControl(),
-    description: new FormControl(),
-    gender: new FormControl('no-answer'),
+    name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    subname: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    description: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(100),
+      Validators.minLength(10),
+    ]),
+    gender: new FormControl('no-answer', [Validators.required]),
   });
 
   onFormSave() {
